5e42776d8efaf61db1d2734b87c2d175c4e6ce91
elizabeth boles <elizabethboles@elizabeths-MacBook-Air.local>
2017-03-20 14:37:53 -0400

    created dinosaur class with methods and static method

--- src/Animals.js
+++ src/Animals.js
@@ -2,7 +2,7 @@
   'use strict';
 
   window.zoo = window.zoo || {};
-  window.zoo.Animals = class Animals {
+  window.zoo.Animal = class Animals {
     constructor(name, dateOfBirth){
       this.name = name;
       console.log('creating a new Animal!');
@@ -12,15 +12,24 @@
       this.dob = dateOfBirth; //pertains to dob and will assign dob to this anim
     }
     changeName(newName){
-      this.name = newName;
+      this.name = newName;//method that allows the name of the animal to change
     }
     getAge(){
       this.age = new Date().getUTCFullYear() - this.dob.getUTCFullYear();
+      //is a method that calculates the age of the animal
+      return this.age;
+    }
+    toString() {//method that returns a string underneath
+      return `This animals name is ${this.name} and it's ${this.getAge()} years old`;
+      //return this.name + ' is a member of ' + this.organization;
+      //lets a user know basic info about the animal
+      //returning a string that uses es6intsyntax
+    }
+  } //end of the class animal
 
-    }
-    toString() {
-      return `This animals name is ${this.name} and it's ${this.age} years old`;
-    }
-  };
+  let animal = new window.zoo.Animal('TIY', new Date('01/01/2000'));
+  console.log(animal);//returned the animal constructor obj dob + age
+  console.log(animal.getAge());//return just the age
+  console.log(animal.toString());//name age years old
 
 }());
